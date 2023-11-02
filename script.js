ef053289e6a49353aa0322635ca9760a80a69480
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 15:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  // Use the "in" operator to check if the key exists in the sampleObject
+  return key in sampleObject;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+const inputKey = prompt("Enter Key.");
+alert(hasKey(inputKey));
